2d6748aa20fcf06a4619c491855c8b0e9f1591d4
Sharmila Gunasekaran <shrmguna18@gmail.com>
2017-03-27 14:46:48 -0700

    Add files via upload

---
+++ myjson.js
@@ -0,0 +1,4 @@
+{"person" : [{"id" : 10 ,"firstname" : "John", "lastname" : "Doe"} ,
+{"id" : 5 ,"firstname" : "Jack", "lastname" : "Doe"},
+{"id" : 7 ,"firstname" : "James", "lastname" : "Doe"}
+]}
